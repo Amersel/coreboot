f3f4daa7ad07f6ed7902ee182f90de519b2e1b48
amersel <amersel@bibanon.org>
2021-04-25 12:10:08 -0400

    source changes for 4.14

--- src/mainboard/lenovo/w541/romstage.c
+++ src/mainboard/lenovo/w541/romstage.c
@@ -22,9 +22,7 @@ void mainboard_config_rcba(void)
 
 void mb_late_romstage_setup(void)
 {
-	u8 enable_peg;
-	if (get_option(&enable_peg, "enable_dual_graphics") != CB_SUCCESS)
-		enable_peg = 0;
+	u8 enable_peg = get_int_option("enable_dual_graphics", 0);
 
 	bool power_en = pmh7_dgpu_power_state();
 
@@ -40,15 +38,15 @@ void mb_late_romstage_setup(void)
 	}
 }
 
-void mb_get_spd_map(uint8_t spd_map[4])
+void mb_get_spd_map(struct spd_info *spdi)
 {
-	spd_map[0] = 0xa0;
-	spd_map[1] = 0xa4;
-	spd_map[2] = 0xa2;
-	spd_map[3] = 0xa6;
+	spdi->addresses[0] = 0x50;
+	spdi->addresses[1] = 0x52;
+	spdi->addresses[2] = 0x51;
+	spdi->addresses[3] = 0x53;
 }
 
-const struct usb2_port_setting mainboard_usb2_ports[MAX_USB2_PORTS] = {
+const struct usb2_port_config mainboard_usb2_ports[MAX_USB2_PORTS] = {
 	/* Length, Enable, OCn#, Location */
 	{ 0x0040, 1, 0, USB_PORT_BACK_PANEL },
 	{ 0x0040, 1, 0, USB_PORT_BACK_PANEL },
@@ -66,7 +64,7 @@ const struct usb2_port_setting mainboard_usb2_ports[MAX_USB2_PORTS] = {
 	{ 0x0040, 1, 6, USB_PORT_BACK_PANEL },
 };
 
-const struct usb3_port_setting mainboard_usb3_ports[MAX_USB3_PORTS] = {
+const struct usb3_port_config mainboard_usb3_ports[MAX_USB3_PORTS] = {
 	{ 1, 0 },
 	{ 1, 0 },
 	{ 1, USB_OC_PIN_SKIP },
