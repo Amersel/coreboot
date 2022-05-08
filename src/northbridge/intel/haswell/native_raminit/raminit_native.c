d041b14f3af69db5f4598c84e3f53c9cd572ffb5
Angel Pons <th3fanbus@gmail.com>
2022-05-08 14:29:05 +0200

    haswell NRI: Add final raminit steps
    Implement the remaining raminit steps. Although many training steps are
    missing, this is enough to boot on the Asrock B85M Pro4.
    
    Change-Id: I94f3b65f0218d4da4fda4d84592dfd91f77f8f21
    Signed-off-by: Angel Pons <th3fanbus@gmail.com>

--- src/northbridge/intel/haswell/native_raminit/raminit_native.c
+++ src/northbridge/intel/haswell/native_raminit/raminit_native.c
@@ -200,8 +200,6 @@ void perform_raminit(const int s3resume)
 		else
 			me_status = ME_INIT_STATUS_SUCCESS;
 
-		/** TODO: Remove this once raminit is implemented **/
-		me_status = ME_INIT_STATUS_ERROR;
 		intel_early_me_init_done(me_status);
 	}
 
@@ -217,7 +215,8 @@ void perform_raminit(const int s3resume)
 	}
 
 	/* Save training data on non-S3 resumes */
-	if (!s3resume)
+	/** TODO: Enable this once training data is populated **/
+	if (0 && !s3resume)
 		save_mrc_data(&md);
 
 	/** TODO: setup_sdram_meminfo **/
