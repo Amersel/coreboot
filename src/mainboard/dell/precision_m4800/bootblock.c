b87831a2239c1341cc965447563dab8e18562fef
Amersel <amersel@runbox.me>
2023-10-18 20:57:03 -0400

    Enable wifi and bluetooth

--- src/mainboard/dell/precision_m4800/bootblock.c
+++ src/mainboard/dell/precision_m4800/bootblock.c
@@ -4,6 +4,7 @@
 #include <ec/dell/mec5035/mec5035.h>
 #include <bootblock_common.h>
 #include <console/console.h>
+#include <option.h>
 #include <southbridge/intel/lynxpoint/pch.h>
 
 void mainboard_config_superio(void)
@@ -12,6 +13,9 @@ void mainboard_config_superio(void)
 		pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x007c0901);
 		mec5035_early_init();
 		mec5035_mouse_touchpad(TP_PS2_MOUSE);
+		mec5035_radio_enable(RADIO_WLAN, get_uint_option("wlan", 1));
+		mec5035_radio_enable(RADIO_WWAN, get_uint_option("wwan", 1));
+		mec5035_radio_enable(RADIO_WPAN, get_uint_option("bluetooth", 1));
 		pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x005c0921);
 		printk(BIOS_DEBUG, "Ending mainboard_config_superio\n");
 
