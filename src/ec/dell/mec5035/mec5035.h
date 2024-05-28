1f9cc17e132d8b7d37e0f8997f70ba83423a326d
Nicholas Chin <nic.c3.14@gmail.com>
2024-05-28 17:23:21 -0600

    ec/dell/mec5035: Replace defines with enums
    Instead of using defines for command IDs and argument values, use enums
    to provide more type safety. This also has the effect of moving the
    command IDs to a more central location instead of defines spread out
    throughout the header.
    
    Change-Id: I788531e8b70e79541213853f177326d217235ef2
    Signed-off-by: Nicholas Chin <nic.c3.14@gmail.com>
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/82998
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    Reviewed-by: Felix Singer <service+coreboot-gerrit@felixsinger.de>

--- src/ec/dell/mec5035/mec5035.h
+++ src/ec/dell/mec5035/mec5035.h
@@ -7,23 +7,28 @@
 
 #define NUM_REGISTERS	32
 
+enum mec5035_cmd {
+	CMD_MOUSE_TP = 0x1a,
+	CMD_RADIO_CTRL = 0x2b,
+	CMD_CPU_OK = 0xc2,
+};
+
 /* Touchpad (TP) and mouse related. The EC seems to
    default to 0 which results in the TP not working. */
-#define CMD_MOUSE_TP	0x1a
-#define SERIAL_MOUSE	0 /* Disable TP, force use of a serial mouse */
-#define PS2_MOUSE	1 /* Disable TP when using a PS/2 mouse */
-#define TP_PS2_MOUSE	2 /* Leave TP enabled when using a PS/2 mouse */
+enum ec_mouse_setting {
+	SERIAL_MOUSE = 0, /* Disable TP, force use of a serial mouse */
+	PS2_MOUSE, /* Disable TP when using a PS/2 mouse */
+	TP_PS2_MOUSE /* Leave TP enabled when using a PS/2 mouse */
+};
 
-#define CMD_CPU_OK	0xc2
-
-#define CMD_RADIO_EN	0x2b
-enum mec5035_radio_dev {
+#define RADIO_CTRL_NUM_ARGS 3
+enum ec_radio_dev {
 	RADIO_WLAN = 0,
 	RADIO_WWAN = 1,
 	RADIO_WPAN = 2,
 };
 
-u8 mec5035_mouse_touchpad(u8 setting);
+u8 mec5035_mouse_touchpad(enum ec_mouse_setting setting);
 void mec5035_cpu_ok(void);
 void mec5035_early_init(void);
 void mec5035_radio_enable(enum mec5035_radio_dev device, u8 on);
